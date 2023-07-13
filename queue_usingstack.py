dab6b3864b49e7d0a603b1e42d90f4fe4c704ecb
Nimit_Gupta <110985866+NimitGupt0786@users.noreply.github.com>
2023-07-13 22:11:50 +0530

    Add files via upload

---
+++ queue_usingstack.py
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/implement-queue-using-stacks/
+from collections import deque
+class MyQueue(object):
+
+    def __init__(self):
+        self.q=deque()      
+
+    def push(self, x):  
+        self.q.append(x)
+
+    def pop(self):
+        return self.q.popleft()
+        
+
+    def peek(self):
+        return self.q[0]
+        
+
+    def empty(self):
+        return len(self.q)==0
